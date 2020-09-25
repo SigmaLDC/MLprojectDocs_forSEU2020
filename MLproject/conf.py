447ee3e5093b67441edfa50c16d89eb912464851
SigmaLDC <51266771+SigmaLDC@users.noreply.github.com>
2020-09-25 23:58:22 +0800

    Update conf.py

--- MLproject/conf.py
+++ MLproject/conf.py
@@ -23,7 +23,7 @@ author = 'Yingtai Li  Yuanhao Yue  Yanglong He  Xiyuan Du'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0'
-
+master_doc="index"
 
 # -- General configuration ---------------------------------------------------
 
